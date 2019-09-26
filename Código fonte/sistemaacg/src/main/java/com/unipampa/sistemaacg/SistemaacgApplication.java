77d921b69400b1eea19c02b8a4272e634e51e61f
Matheus Marques <sandro-matheus@hotmail.com>
2019-09-26 19:11:21 -0300

    Setando o projeto

---
+++ Código fonte/sistemaacg/src/main/java/com/unipampa/sistemaacg/SistemaacgApplication.java
@@ -0,0 +1,13 @@
+package com.unipampa.sistemaacg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaacgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaacgApplication.class, args);
+	}
+
+}

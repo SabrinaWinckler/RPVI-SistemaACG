d82c4ca371d8145af46830afd5ffc7f59e5d9281
Sabrina Winckler <sabrinacarlew@gmail.com>
2019-09-26 21:15:49 -0300

    Added Métodos Anexo e Solicitação

--- Código fonte/sistemaacg/src/main/java/com/unipampa/sistemaacg/models/Solicitacao.java
+++ Código fonte/sistemaacg/src/main/java/com/unipampa/sistemaacg/models/Solicitacao.java
@@ -1,5 +1,6 @@
 package com.unipampa.sistemaacg.models;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.persistence.Entity;
@@ -36,18 +37,37 @@ public class Solicitacao {
 	@NotEmpty
 	private boolean status;
 
-	
 	private String profRes;
 
 
 	private String descricao;
 
-	public void incluir() {
+	public boolean verificaTamanho(int tamanho){
+        return tamanho <= 20;
+    }
 
+    public void incluirSolicitacao(String dataAtual, String dataInicio, String dataFim, int ch, String descricao,
+			String profRes, String nomeAnexo, int tamAnexo, int idAtividade, int idGrupo, int idCurriculo)
+			throws Exception {
+        if(!verificaTamanho(tamAnexo)){
+            throw new Exception("Tamanho do anexo excede 20mb");
+        }
+        Anexo anexo = new Anexo(nomeAnexo);
+
+        SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
+
+        this.dataAtual = formato.parse(dataAtual);
+        this.dataInicio = formato.parse(dataInicio);
+        this.dataFim = formato.parse(dataFim);
+
+        this.cargaHorariaSoli = ch;
+        this.descricao = descricao;
+        this.profRes = profRes;
+
+        repository.save(this);
 	}
 
 	public Solicitacao excluir() {
 		return null;
 	}
-
 }

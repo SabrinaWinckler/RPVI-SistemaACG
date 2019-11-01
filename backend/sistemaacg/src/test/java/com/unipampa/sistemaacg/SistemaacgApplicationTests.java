73551696af368c5f800beedcab6fb8582b2323db
Sabrina Winckler <sabrinacarlew@gmail.com>
2019-10-31 21:30:10 -0300

    backend

---
+++ backend/sistemaacg/src/test/java/com/unipampa/sistemaacg/SistemaacgApplicationTests.java
@@ -0,0 +1,19 @@
+package com.unipampa.sistemaacg;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SistemaacgApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
